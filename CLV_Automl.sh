0a0aab352d27e1f770eb418b326dadbff0680953
Sufyaan Kazi <sufyaankazi@google.com>
2019-07-30 23:42:04 +0100

    Updates

--- CLV_Automl.sh
+++ CLV_Automl.sh
@@ -124,7 +124,7 @@ main() {
   cp $KEY_FILE ${LOCAL_FOLDER}/clv_automl
   cd ${LOCAL_FOLDER}/clv_automl
   cp clv_automl.py clv_automl.orig
-  cat clv_automl.py | sed -e 's/us-central1/europe-west1/g' > clv_automl.py
+  cat clv_automl.orig | sed -e 's/us-central1/europe-west1/g' > clv_automl.py
   cat clv_automl.py
   python clv_automl.py --project_id ${PROJECT}
 }
