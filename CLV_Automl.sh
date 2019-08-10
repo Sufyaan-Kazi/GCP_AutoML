f483d0b93f14cfb85955262d9205bcc4711073b5
Sufyaan Kazi <sufyaankazi@google.com>
2019-08-10 15:27:32 +0100

    Updates

--- CLV_Automl.sh
+++ CLV_Automl.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Copyright 2018 Google LLC. This software is provided as-is, without warranty or representation for any use or purpose. Your use of it is subject to your agreements with Google.  
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
