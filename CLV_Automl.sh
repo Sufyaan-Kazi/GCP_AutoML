1657e80ba4d946675239b1d52fb0e49292062df3
Sufyaan Kazi <sufyaankazi@google.com>
2019-08-10 01:38:34 +0100

    Updates

--- CLV_Automl.sh
+++ CLV_Automl.sh
@@ -120,8 +120,8 @@ main() {
   bq --location=US mk --dataset ${PROJECT}:${DATASET_NAME}
   bq mk -t --schema ../data_source.json ${PROJECT}:${DATASET_NAME}.${TABLE_NAME}
   bq --location=US load --source_format=CSV ${PROJECT}:${DATASET_NAME}.${TABLE_NAME} ${BUCKET}/db_dump.csv
-  bq query --destination_table ${PROJECT}:${DATASET_NAME}.data_cleaned --use_legacy_sql=false --quiet < ../clean.sql
-  bq query --destination_table ${PROJECT}:${DATASET_NAME}.features_n_target --use_legacy_sql=false --quiet < ../features_n_target.sql
+  bq query --destination_table ${PROJECT}:${DATASET_NAME}.data_cleaned --use_legacy_sql=false < ../clean.sql
+  bq query --destination_table ${PROJECT}:${DATASET_NAME}.features_n_target --use_legacy_sql=false < ../features_n_target.sql
 
   #train using AutoML
   cp $KEY_FILE ${LOCAL_FOLDER}/clv_automl
